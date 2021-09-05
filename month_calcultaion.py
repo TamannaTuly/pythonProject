37dab15caedf4082c99a946ff4616c3d2a65a565
TamannaTuly <ttniger53@gmail.com>
2021-09-06 00:50:47 +0600

    CALENDAR shorten with sets- exercise

--- month_calcultaion.py
+++ month_calcultaion.py
@@ -2,14 +2,21 @@ month = input('Enter the month name with first 3 words : ').lower()
 
 def num_days(month='jan'):
 
-    if month == 'jan' or month=='dec' or month=='oct' or month=='aug' or month == 'jul' or month=='may' or month=='mar':
-        print('number of days in',month,'is',31)
-    elif month == 'feb':
-        print('number of days in',month,'is',28)
-    elif month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
-        print('number of days in',month,'is',30)
-    else:
-        print('Invalid month name')
+    # if month == 'jan' or month=='dec' or month=='oct' or month=='aug' or month == 'jul' or month=='may' or month=='mar':
+    #     print('number of days in',month,'is',31)
+    # elif month == 'feb':
+    #     print('number of days in',month,'is',28)
+    # elif month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
+    #     print('number of days in',month,'is',30)
+    # else:
+    #     print('Invalid month name')
+    days = 31
+    if month in {'apr','jun','sep' ,'nov'}:
+        days =30
+    elif month in {'feb'}:
+        days =28
+    print(f"number of {month}\'s is {days} days")
+
 num_days(month)
 # optimize/shorten the code in the function
 # try to reduce the number of conditionals
