0b8ed72b5b8acc0921fcd2aee5e3bf853234066b
TamannaTuly <ttniger53@gmail.com>
2021-12-27 14:43:55 +0600

    dictionaries - test 01

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,16 @@
 char = {
     'name' : 'tuly',
-    'test' : 'python'
+    'test' : 'python',
+    'id' : {10,22}
 }
 # print(char)
 # print(char['name'])
 # print(char['what'])#will throw error
 # print(char.get('doesnotexist'))#will return none
 print(char.get('doesnotexist','customizedMessage : Not Found'))
+print(char.get('firstName','Not Found'))
 print('test')
+
+char['firstName']='tamanna'
+print(char.get('firstName'))
+print(char)
